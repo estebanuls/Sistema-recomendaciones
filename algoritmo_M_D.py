ec55ebaa3a6bfcc052394cf6ef46debb415958c3
Berni-wq <bcasangagallardo@gmail.com>
2026-04-15 23:43:41 -0400

    Implement file saving and size checking for dataset
    comvierte la estructura de datos, la pasa a un archivo, pregunta el tamaño del archivo y lo pasa a megabits pq los megabits son dificiles de leer.

--- algoritmo_M_D.py
+++ algoritmo_M_D.py
@@ -6,6 +6,13 @@ import pandas as pd
 import os
 
 nombre_archivo = 'archivo dataset' #aqui se necesita el archivo
+sparse.save_npz(nombre_archivo, sparse_matrix)
+
+# Ver cuánto pesa el archivo en el disco
+tamaño = os.path.getsize(nombre_archivo) / (1024 * 1024)#pregunta cuanto pesa el archivo guardado y conbierte los bites ah mega (convergencia)
+print(f"Archivo guardado: {nombre_archivo}")
+print(f"Peso en disco: {tamaño:.2f} MB")
 
 #iniciar la carga de la base de datos
 df = pd.read_csv('archivo dataset')
+
