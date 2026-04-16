fe0fcb8f66e3737032ac2b10b16d5a079540330e
Berni-wq <bcasangagallardo@gmail.com>
2026-04-15 23:51:41 -0400

    Implement sparse matrix creation for ratings
    en resumen se tomara el archivo csv y convertimos los ID EN coordenadas y los valores que esten vacios no los toma

--- algoritmo_M_D.py
+++ algoritmo_M_D.py
@@ -16,3 +16,19 @@ print(f"Peso en disco: {tamaño:.2f} MB")
 #iniciar la carga de la base de datos
 df = pd.read_csv('archivo dataset')
 
+#Crear categorías para manejar los IDs como índices
+# Esto mapea los IDs originales a rangos de 0 a N-1
+user_u = list(df.userId.unique())
+movie_u = list(df.movieId.unique())
+
+data = df['rating'].tolist()
+row = df.userId.astype('category').cat.codes
+col = df.movieId.astype('category').cat.codes
+
+#Se construye la matriz dispersa (formato CSR)
+sparse_matrix = csr_matrix((data, (row, col)), shape=(len(user_u), len(movie_u)))
+
+print(f"Dimensiones de la matriz: {sparse_matrix.shape}")
+print(f"Elementos no nulos: {sparse_matrix.nnz}")
+
+
