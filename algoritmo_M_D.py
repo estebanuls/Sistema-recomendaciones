3b38b8b44a400eb138a278dafcbff07835f22946
Berni-wq <bcasangagallardo@gmail.com>
2026-04-15 23:12:55 -0400

    Import libraries and load dataset in algoritmo_M_D.py
    aplicaremos estas librerias para matriz dispersa y se inicializara el archivo dataset dado

--- algoritmo_M_D.py
+++ algoritmo_M_D.py
@@ -1,1 +1,11 @@
 #codigo
+#librerias ha usar
+from scipy.sparse import csr_matrix
+from scipy import sparse
+import pandas as pd
+import os
+
+nombre_archivo = 'archivo dataset' #aqui se necesita el archivo
+
+#iniciar la carga de la base de datos
+df = pd.read_csv('archivo dataset')
